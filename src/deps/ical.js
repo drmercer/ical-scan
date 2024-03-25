6d9baf0efa6d633f49fe59a95114470bc247296d
Dan Mercer <danielmercer1@gmail.com>
2024-03-25 17:49:20 -0600

    0.0.5 add a dependency

---
+++ src/deps/ical.js
@@ -0,0 +1,2 @@
+// @ts-ignore No types for ical.js package :(
+export * as ICAL from 'ical.js';
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+import { ICAL } from "./deps/ical.js";
+
 /**
  * @returns {string} A friendly message
  */
 export function helloWorld() {
+  console.log(ICAL);
   return 'Hello World!';
 }
